5845ea5056c8068afd17337d182f60a1e4edc71d
Pooch <john.pucciarelli@gmail.com>
2011-06-27 20:21:34 -0400

    updated view launcher

--- ProjectLoud/src/co/shoutbreak/ui/SBContext.java
+++ ProjectLoud/src/co/shoutbreak/ui/SBContext.java
@@ -16,64 +16,64 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 public class SBContext extends Activity {
-	
+
 	private final static String TAG = "Launcher.java";
 
 	private SBNotificationManager _NotificationManager;
 	private ServiceBridgeInterface _ServiceBinder;
 	private SBView _ViewArray[];
 	private SBView _currentView;
-	
+
 	/* LIFECYCLE METHODS */
-	
+
 	@Override
-    public void onCreate(Bundle bundle) {
-    	SBLog.i(TAG, "onCreate()");
-        super.onCreate(bundle);
+	public void onCreate(Bundle bundle) {
+		SBLog.i(TAG, "onCreate()");
+		super.onCreate(bundle);
 
-        setContentView(R.layout.main);
-        
-        // initialize components
-        _NotificationManager = new SBNotificationManager(this);
-        
-        // set current view
-        _ViewArray = new SBView[3];
-        _ViewArray[0] = new ComposeView(SBContext.this, "Send Shout", R.id.compose_view, 0);
-        _ViewArray[1] = new InboxView(SBContext.this, "Inbox", R.id.inbox_view, 1);
-        _ViewArray[2] = new ProfileView(SBContext.this, "Profile", R.id.profile_view, 2);
-       	switchView(_ViewArray[0]);
-       	switchView(_ViewArray[2]);
-        
-        // connect to service
-        Intent serviceIntent = new Intent(SBContext.this, SBService.class);
-        serviceIntent.putExtra(C.START_FROM_UI, true);
-        startService(serviceIntent); // must be called, BIND_AUTO_CREATE doesn't start service
-        bindService(serviceIntent, _ServiceConnection, Context.BIND_AUTO_CREATE);
-    }
-    
-    private ServiceConnection _ServiceConnection = new ServiceConnection() {
-    	
-    	public void onServiceConnected(ComponentName className, IBinder service) {
-    		SBLog.i(TAG, "onServiceConnected()");
-    		_ServiceBinder = (ServiceBridgeInterface) service;
-    		_ServiceBinder.getStateManager().enableData();
-    		_ServiceBinder.getStateManager().disableData();
-    		_ServiceBinder.getStateManager().enableData();    		
-    	}
+		setContentView(R.layout.main);
 
-    	public void onServiceDisconnected(ComponentName className) {
-    		// should never be called
-    		SBLog.i(TAG, "onServiceDisconnected()");
-    	}
-    	
-    };
+		// initialize components
+		_NotificationManager = new SBNotificationManager(this);
 
-    @Override
+		// set current view
+		_ViewArray = new SBView[3];
+		_ViewArray[0] = new ComposeView(SBContext.this, "Send Shout", R.id.compose_view, 0);
+		_ViewArray[1] = new InboxView(SBContext.this, "Inbox", R.id.inbox_view, 1);
+		_ViewArray[2] = new ProfileView(SBContext.this, "Profile", R.id.profile_view, 2);
+		switchView(_ViewArray[0]);
+		switchView(_ViewArray[2]);
+
+		// connect to service
+		Intent serviceIntent = new Intent(SBContext.this, SBService.class);
+		serviceIntent.putExtra(C.START_FROM_UI, true);
+		startService(serviceIntent); // must be called, BIND_AUTO_CREATE doesn't start service
+		bindService(serviceIntent, _ServiceConnection, Context.BIND_AUTO_CREATE);
+	}
+
+	private ServiceConnection _ServiceConnection = new ServiceConnection() {
+
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			SBLog.i(TAG, "onServiceConnected()");
+			_ServiceBinder = (ServiceBridgeInterface) service;
+			_ServiceBinder.getStateManager().enableData();
+			_ServiceBinder.getStateManager().disableData();
+			_ServiceBinder.getStateManager().enableData();
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// should never be called
+			SBLog.i(TAG, "onServiceDisconnected()");
+		}
+
+	};
+
+	@Override
 	public void onNewIntent(Intent intent) {
-    	SBLog.i(TAG, "onNewIntent()");
+		SBLog.i(TAG, "onNewIntent()");
 		_NotificationManager.handleNotificationExtras(intent.getExtras());
 	}
-    
+
 	@Override
 	protected void onStart() {
 		SBLog.i(TAG, "onStart()");
@@ -103,14 +103,14 @@ public class SBContext extends Activity {
 	@Override
 	protected void onDestroy() {
 		SBLog.i(TAG, "onDestroy()");
-		
+
 		unbindService(_ServiceConnection);
 		_ServiceBinder = null;
 		super.onDestroy();
 	}
-	
+
 	/* VIEW METHODS */
-	
+
 	public void switchView(SBView view) {
 		if (_currentView != null) {
 			_currentView.hide();
